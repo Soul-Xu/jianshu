bd500a1a0158d93a468d813b54e9706e5c55d03d
Soul-Xu <741930067@qq.com>
2018-08-11 19:38:03 +0800

    jianshu

---
+++ src/common/header/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { actionCreators } from './store'
+import { CSSTransition } from 'react-transition-group'
+import { HeaderWrapper,
+         Logo,
+         Nav,
+         NavItem,
+         SearchWrapper,
+         NavSearch,
+         Addition,
+         Button,
+} from './style'
+
+class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登陆</NavItem>
+          <NavItem className='right'>
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={this.props.focused}
+              timeout={200}
+              classNames='slide'>
+              <NavSearch
+                className={this.props.focused ? 'focused': ''}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+                <i className={this.props.focused ? 'focused iconfont': 'iconfont'}>&#xe60d;</i>
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writting'>
+            <i className="iconfont">&#xe600;</i>
+            写文章
+          </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.header.focused
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      dispatch(actionCreators.searchFocus())
+    },
+    handleInputBlur() {
+      dispatch(actionCreators.searchBlur())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
+
